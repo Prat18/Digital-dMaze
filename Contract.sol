315df464ece8e655665dee36a68ef61ba0f25bea
Shashwat Hiregodar <shashwathiregoudar@gmail.com>
2018-10-19 14:18:16 +0530

    intial commit

---
+++ Contract.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.19;
+contract Dmaze {
+
+    //There are many many approaches to generate random mazes
+    // There are a few examples 
+    function _createMap()private {
+
+    }
+    function _spawnPlayer()private {
+        
+    }
+    function _getNextBlock(uint curXPos,uint curYPos)private {
+        
+    }
+}
